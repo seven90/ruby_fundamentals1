4be22ef2c88b0c1e34e180196931748a0489b002
Seven90 <stevesurya@gmail.com>
2014-06-03 11:59:23 -0400

    Exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,8 @@
+#tip calculation for $55
+tip = 0.2 * 55
+puts "The proper tip on a $55 meal is $#{tip}."
+#adding a an interger to a string
+add = "Name" + 5.to_s
+puts add
+#interpolation
+puts "The result of multiplying 45628 by 7839 is #{45628*7839}."
